dc1b523dc80eb8b1ede37092d257f3985658f67f
Ian Walker <iantdwalker@gmail.com>
2023-09-06 10:23:05 +0100

    Updated build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
 import { fileURLToPath, URL } from "node:url";
-
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
+import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  base: "/TheSimpsonsEpisodeRandomizer/",
+  plugins: [
+    vue({
+      template: { transformAssetUrls },
+    }),
+    vueJsx(),
+    vuetify({ autoImport: true }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
