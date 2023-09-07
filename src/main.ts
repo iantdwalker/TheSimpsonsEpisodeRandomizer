9dc01ae4513d9560a78e2c5bc56a0c038310423b
Ian Walker <iantdwalker@gmail.com>
2023-09-07 14:23:03 +0100

    Added the vuetify plugin

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,19 @@
 import "./assets/main.css";
-
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-
 import App from "./App.vue";
 import router from "./router";
+
+// add vuetify styles
+import "vuetify/styles";
+
+//add vuetify plugin
+import createVuetify from "@/plugins/vuetify";
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(createVuetify);
 
 app.mount("#app");
