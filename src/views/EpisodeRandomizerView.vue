c3d01af2d87436d95590621b53b2e16e63542efe
Ian Walker <iantdwalker@gmail.com>
2023-09-07 14:23:23 +0100

    WIP on page design

--- src/views/EpisodeRandomizerView.vue
+++ src/views/EpisodeRandomizerView.vue
@@ -1,10 +1,42 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-layout>
+    <v-container class="container">
+      <v-main class="main">
+        <v-row no-gutters>
+          <v-col>
+            <img
+              alt="Simpsons Image"
+              src="@/assets/images/the-simpsons.png"
+              width="600"
+              height="125"
+            />
+          </v-col>
+        </v-row>
+      </v-main>
+    </v-container>
+  </v-layout>
 </template>
 
+<script setup lang="ts">
+import { computed, ref, onBeforeMount, watch } from "vue";
+import { useEpisodeStore } from "@/stores/episode";
+
+var episodeStore = useEpisodeStore();
+
+onBeforeMount(async () => {
+  await episodeStore.importEpisodeData();
+  episodeStore.initialiseEpisodeData();
+  //hintText.value = questionStore.currentQuestion.hints.toString();
+  //questionIcon.value = questionStore.currentQuestion.icon;
+});
+</script>
+
 <style>
+.container {
+  min-height: 100vh;
+  background-color: #4593E2;
+}
+
 @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
